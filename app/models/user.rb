f3b77bc583408826a5a90373f9dca75380c0d315
kakeru <okinawan.kkr0713@icloud.com>
2019-10-29 17:49:57 +0900

    バリデーション追記、スキーマファイル書換

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  has_many :feeds
+  validates :name, presence: true
+  validates :email,presence: true, uniqueness: true,
+                  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :image, presence: true
+  mount_uploader :image, ImageUploader
+  before_validation { email.downcase! }
+  has_secure_password
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_071018) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
