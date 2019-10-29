43597cf870e47b4e8906615906eb2ab619bd4e03
kakeru <okinawan.kkr0713@icloud.com>
2019-10-29 18:22:38 +0900

    controller生成、routes追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
+  # resources :favorites, only: [:create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  # resources :feeds do
+  #   collection do
+  #     post :confirm
+  #   end
+  # end
 end
