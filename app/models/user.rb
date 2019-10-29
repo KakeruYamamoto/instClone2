5904ceb662946801ba83a8b0cd9a12078b7f3791
kakeru <okinawan.kkr0713@icloud.com>
2019-10-29 17:51:03 +0900

    バリデーション書換

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :feeds
-  validates :name, presence: true
-  validates :email,presence: true, uniqueness: true,
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email,presence: true, length: { maximum: 255 }, uniqueness: true,
                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
   validates :image, presence: true
