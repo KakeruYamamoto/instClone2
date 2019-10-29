81a1d70bd330d709b0f7581b1849a447a220f117
kakeru <okinawan.kkr0713@icloud.com>
2019-10-29 20:00:55 +0900

    session,user記述,veiw生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to: 'sessions#new'
   # resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
