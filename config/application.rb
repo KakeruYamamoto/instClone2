365337fdd3582873143b4f1a7af568475bb5e86c
kakeru <okinawan.kkr0713@icloud.com>
2019-10-29 17:03:54 +0900

    タイムゾーン記述

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,21 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module InstClone
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.generators do |g|
+       g.assets false
+       g.helper false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
