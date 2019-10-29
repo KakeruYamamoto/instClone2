516a4598a332c500300ded32f28e44136de5859c
kakeru <okinawan.kkr0713@icloud.com>
2019-10-29 19:16:45 +0900

    アクション追記、view/new,show生成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+       redirect_to user_path(@user.id)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+     @user = User.find(params[:id])
+     # @favorite = current_user.favorites.find_by(feed_id: @feed.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
 end
