296a119aed3a63e0fc95b486ae24d7d4dc9fa67c
kakeru <okinawan.kkr0713@icloud.com>
2019-10-30 19:24:49 +0900

    アクション内容を修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
 
   def create
-    favorite = current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
+    favorite = current_user.favorites.create(feed_id: params[:feed_id])
+    redirect_to feeds_url, notice: "#{favorite.feed.user.name}さんのブログをお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り解除しました"
+    redirect_to feeds_url, notice: "#{favorite.feed.user.name}さんのブログをお気に入り解除しました"
   end
-  
+
 end
