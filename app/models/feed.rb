f15d11fb79f6c5749a4c2131cd5139507669076e
kakeru <okinawan.kkr0713@icloud.com>
2019-10-30 14:25:09 +0900

    バリデーション、アソシエーション、mount_uploader記述

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,2 +1,8 @@
 class Feed < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
+  mount_uploader :image, ImageUploader
+  belongs_to :user
+  validates :image, presence: true
+  validates :content,  presence: true
 end
