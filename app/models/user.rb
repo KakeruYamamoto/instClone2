3ce9eb5428207fb7e19aaf7813bf693aacf87132
kakeru <okinawan.kkr0713@icloud.com>
2019-10-30 14:44:14 +0900

    modelにImageUploader追記、レイアウト調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
   # validates :image, presence: true
-  # mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
   before_validation { email.downcase! }
   has_secure_password
 
