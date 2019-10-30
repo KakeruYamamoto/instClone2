9fd7c56b7e5e55030446949fdcdeb9fe3244df61
kakeru <okinawan.kkr0713@icloud.com>
2019-10-30 14:18:15 +0900

    scaffoldでファイル一式作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :feeds
   get 'sessions/new'
   root to: 'sessions#new'
   # resources :favorites, only: [:create, :destroy]
---
+++ db/migrate/20191030051358_create_feeds.rb
@@ -0,0 +1,10 @@
+class CreateFeeds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :feeds do |t|
+      t.text :image
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
