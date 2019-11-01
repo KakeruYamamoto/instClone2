8057aa7f4b3d64960404798b9095cf938bb4b81a
kakeru <okinawan.kkr0713@icloud.com>
2019-11-01 15:50:46 +0900

    ContactMailer.contact_mail(@contact).deliver記述

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -28,6 +28,7 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
+        ContactMailer.contact_mail(@contact).deliver
         format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
         format.json { render :show, status: :created, location: @contact }
       else
