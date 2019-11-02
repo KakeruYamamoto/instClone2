a9cdcd861df585557519fc0609c0dca8846ba28d
kakeru <okinawan.kkr0713@icloud.com>
2019-11-02 17:12:27 +0900

    seed記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   validates :email,presence: true, length: { maximum: 255 }, uniqueness: true,
                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
-  validates :image, presence: true
   mount_uploader :image, ImageUploader
   before_validation { email.downcase! }
   has_secure_password
