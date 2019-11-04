fa1c7dae2577c635c514f18323d3a59ee56c8410
kakeru <okinawan.kkr0713@icloud.com>
2019-11-04 12:59:49 +0900

    バリデーション追記

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email,  presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :content,  presence: true
 end
